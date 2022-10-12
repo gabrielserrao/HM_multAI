f3ad0d8569e3088dc5c7a2555eb1622dfe61bfc7
gabriel <gabriel.serrao.seabra@gmail.com>
2022-10-12 12:44:01 +0200

    try slurm

--- FlowSimulation/HM/slurm_py.py
+++ FlowSimulation/HM/slurm_py.py
@@ -2,19 +2,19 @@
 
 import os
 
-#def mkdir_p(dir):
-#    '''make a directory (dir) if it doesn't exist'''
-#    if not os.path.exists(dir):
-#        os.mkdir(dir)
+def mkdir_p(dir):
+   '''make a directory (dir) if it doesn't exist'''
+   if not os.path.exists(dir):
+       os.mkdir(dir)
     
 
 job_directory = os.getcwd()
 
-#data_dir = os.path.join(job_directory, '/project')
+data_dir = os.path.join(job_directory, '/project')
 
 # Make top level directories
-#mkdir_p(job_directory)
-#mkdir_p(data_dir)
+mkdir_p(job_directory)
+mkdir_p(data_dir)
 
 lizards=["LizardA","LizardB"]
 
@@ -24,13 +24,17 @@ for lizard in lizards:
     with open(job_file, 'w+') as fh:
         fh.writelines("#!/bin/bash\n")
         fh.writelines("#SBATCH --job-name=%s.job\n" % lizard)
-        fh.writelines("#SBATCH --output=.out/%s.out\n" % lizard)
-        fh.writelines("#SBATCH --error=.out/%s.err\n" % lizard)
+        fh.writelines("#SBATCH --output=%s.out\n" % lizard)
+        fh.writelines("#SBATCH --error=%s.err\n" % lizard)
         fh.writelines("#SBATCH --time=2:00:00\n")
         fh.writelines("#SBATCH --mem=1G\n")
         fh.writelines("#SBATCH --partition=compute\n")
         fh.writelines("#SBATCH --account=research-ceg-gse\n")
-        fh.writelines("python teste_gpu.py $SLURM_ARRAY_TASK_ID $SLURM_SUBMIT_DIR\n")
+        fh.writelines(f'a={str(lizard)}\n')
+        ##fh.writelines(f'b={str(destDir)}\n')
+        ##fh.writelines(f'c={str(time_range[-1])}\n')
+        fh.writelines(f'python teste_gpu.py $a\n')
+        ##fh.writelines("python teste_gpu.py $SLURM_SUBMIT_DIR\n")
 
     os.system("sbatch %s" %job_file)
 
